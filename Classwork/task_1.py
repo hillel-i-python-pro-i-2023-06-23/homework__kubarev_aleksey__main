374e82c32012e3c2ea9199e001a32276fadc2d1a
dissna <alex.kub.05@gmail.com>
2023-06-27 21:16:04 +0300

    Update task_1.py

--- Classwork/task_1.py
+++ Classwork/task_1.py
@@ -1,7 +1,16 @@
 players = ["Игрок 1", "Игрок 2"]
-used_words = ["Лак"]
+current_player_index = 0
+used_words = []
 
-def is_word_valid(used_words, new_word):
+
+def get_current_player():
+    return players[current_player_index]
+
+def change_turn():
+    global current_player_index
+    current_player_index = (current_player_index + 1) % len(players)
+
+def is_word_valid(new_word):
     if new_word in used_words:
         return False
     else:
@@ -21,33 +30,34 @@ def is_word_valid(used_words, new_word):
 
 def play_game():
     print("===== Игра слов =====")
-    print("Введите '!правила' для получения правил игры.")
     print("Введите '!выход' для завершения игры.")
+    print("Введите '!помощь' для получения правил игры.")
     print()
 
     while True:
-        word = input(f"[Player1] Введите слово: ")
-        is_word_valid(used_words, word)
-        used_words.append(word)
-        print(f"Игрок ввел слово '{word}'.")
-        word = input(f"[Player2] Введите слово: ")
-        is_word_valid(used_words, word)
-        used_words.append(word)
-        print(f"Игрок ввел слово '{word}'.")
+        current_player = get_current_player()
+        word = input(f"[{current_player}] Введите слово: ")
 
         if word == "!выход":
             print("Игра завершена.")
             break
-        elif word == "!правила":
+        elif word == "!помощь":
             print("Правила игры:")
             print("  - Игроки должны вводить слова по очереди.")
             print("  - Последняя буква предыдущего слова должна совпадать с первой буквой следующего слова.")
             print("  - Запрещено использовать ранее использованные слова.")
             print("  - Если последняя буква слова 'ь', 'й', 'ы' или 'ъ', следующее слово должно начинаться с предыдущей буквы.")
-            print()
             continue
+
+        if not is_word_valid(word):
+            print("Слово недопустимо. Попробуйте снова.")
+            continue
+
+        used_words.append(word)
+        print(f"Игрок {current_player} ввел слово '{word}'.")
+        change_turn()
 
     print("Игра завершена.")
 
-
+# Запуск игры
 play_game()
